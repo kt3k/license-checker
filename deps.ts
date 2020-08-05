4844fa99ec9578afd08f1d5f4705b99643114fbe
Yoshiya Hinosawa <stibium121@gmail.com>
2020-08-05 22:09:31 +0900

    chore: update versions

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Copyright 2020 Yoshiya Hinosawa. All rights reserved. MIT license.
 
-export { parse } from "https://deno.land/std@v0.58.0/flags/mod.ts";
-export { red, green, blue } from "https://deno.land/std@v0.58.0/fmt/colors.ts";
-export { expandGlob } from "https://deno.land/std@v0.58.0/fs/mod.ts";
-export { posix, win32 } from "https://deno.land/std@v0.58.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.63.0/flags/mod.ts";
+export { red, green, blue } from "https://deno.land/std@0.63.0/fmt/colors.ts";
+export { expandGlob } from "https://deno.land/std@0.63.0/fs/mod.ts";
+export { posix, win32 } from "https://deno.land/std@0.63.0/path/mod.ts";
