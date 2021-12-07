1a06b77738e7a71dee38479a3dddf2d559a26f35
Yoshiya Hinosawa <stibium121@gmail.com>
2021-12-07 16:26:33 +0900

    fix: fix import path

--- build_npm.ts
+++ build_npm.ts
@@ -1,5 +1,5 @@
 import { build } from "https://deno.land/x/dnt@0.7.4/mod.ts";
-import { copy } from "https://deno.land/std@0.117.0/fs/mod.ts";
+import { copy } from "https://deno.land/std@0.117.0/fs/copy.ts";
 
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 await copy("testdata", "npm/esm/testdata", { overwrite: true });
