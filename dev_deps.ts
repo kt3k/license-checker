faa8bfc14ea31167befdec75fc1ce576138e98a7
Yoshiya Hinosawa <stibium121@gmail.com>
2022-01-31 20:16:54 +0900

    chore: update deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,5 +1,5 @@
 // Copyright 2020 Yoshiya Hinosawa. All rights reserved. MIT license.
 
-export { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-export { StringReader } from "https://deno.land/std@0.117.0/io/readers.ts";
+export { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
+export { StringReader } from "https://deno.land/std@0.123.0/io/readers.ts";
 export { isNode } from "https://deno.land/x/which_runtime@0.2.0/mod.ts";
