b78439bb648b4356f911aee8a9950c5f7fed96fa
Yoshiya Hinosawa <stibium121@gmail.com>
2022-10-31 18:53:48 +0900

    refactor: use path.relative

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@
 export { parse } from "https://deno.land/std@0.161.0/flags/mod.ts";
 export { blue, green, red } from "https://deno.land/std@0.161.0/fmt/colors.ts";
 export { expandGlob } from "https://deno.land/std@0.160.0/fs/mod.ts";
-export { posix, win32 } from "https://deno.land/std@0.161.0/path/mod.ts";
+export { relative } from "https://deno.land/std@0.161.0/path/mod.ts";
 export { includesNeedle as contains } from "https://deno.land/std@0.161.0/bytes/mod.ts";
 export { copy } from "https://deno.land/std@0.161.0/streams/conversion.ts";
 export { delay } from "https://deno.land/std@0.161.0/async/delay.ts";
