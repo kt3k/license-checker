d10ccfda2c1031fde02d2d74ef32a38d9e2b6929
Yoshiya Hinosawa <stibium121@gmail.com>
2022-10-31 17:35:17 +0900

    chore: update deps

--- dnt.ts
+++ dnt.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2022 Yoshiya Hinosawa. All rights reserved. MIT license.
 
 import { build } from "https://deno.land/x/dnt@0.16.1/mod.ts";
-import { copy } from "https://deno.land/std@0.123.0/fs/copy.ts";
+import { copy } from "https://deno.land/std@0.161.0/fs/copy.ts";
 
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 await copy("testdata", "npm/esm/testdata", { overwrite: true });
