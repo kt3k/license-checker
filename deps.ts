d62d58661d59b7cc75e60bf4816797dd972ba198
Yoshiya Hinosawa <stibium121@gmail.com>
2024-01-12 13:23:36 +0900

    chore: switch to jsr

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 // Copyright 2020-2022 Yoshiya Hinosawa. All rights reserved. MIT license.
 
-export { parseArgs as parse } from "https://deno.land/std@0.211.0/cli/parse_args.ts";
-export { blue, green, red } from "https://deno.land/std@0.211.0/fmt/colors.ts";
-export { expandGlob } from "https://deno.land/std@0.211.0/fs/expand_glob.ts";
-export { relative } from "https://deno.land/std@0.211.0/path/relative.ts";
-export { includesNeedle as contains } from "https://deno.land/std@0.211.0/bytes/includes_needle.ts";
-export { copy } from "https://deno.land/std@0.211.0/streams/copy.ts";
-export { delay } from "https://deno.land/std@0.211.0/async/delay.ts";
+export { parseArgs as parse } from "jsr:@std/cli@0.209.1/parse_args";
+export { blue, green, red } from "jsr:@std/fmt@0.210.0/colors";
+export { expandGlob } from "jsr:@std/fs@0.209.1/expand_glob";
+export { relative } from "jsr:@std/path@0.209.1/relative";
+export { includesNeedle as contains } from "jsr:@std/bytes@0.209.1/includes_needle";
+export { copy } from "jsr:@std/streams@0.209.1/copy";
+export { delay } from "jsr:@std/async@0.209.1/delay";
