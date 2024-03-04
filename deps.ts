33335df67b7d1c789cd0f57941157d0d8862c460
Yoshiya Hinosawa <stibium121@gmail.com>
2024-03-04 14:23:02 +0900

    upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 // Copyright 2020-2022 Yoshiya Hinosawa. All rights reserved. MIT license.
 
-export { parseArgs as parse } from "jsr:@std/cli@0.209.1/parse_args";
-export { blue, green, red } from "jsr:@std/fmt@0.210.0/colors";
-export { expandGlob } from "jsr:@std/fs@0.209.1/expand_glob";
-export { relative } from "jsr:@std/path@0.209.1/relative";
-export { includesNeedle as contains } from "jsr:@std/bytes@0.209.1/includes_needle";
-export { copy } from "jsr:@std/streams@0.209.1/copy";
-export { delay } from "jsr:@std/async@0.209.1/delay";
+export { parseArgs as parse } from "jsr:@std/cli@0.218.0/parse_args";
+export { blue, green, red } from "jsr:@std/fmt@0.218.0/colors";
+export { expandGlob } from "jsr:@std/fs@0.218.0/expand_glob";
+export { relative } from "jsr:@std/path@0.218.0/relative";
+export { includesNeedle as contains } from "jsr:@std/bytes@0.218.0/includes_needle";
+export { copy } from "jsr:@std/io@0.218.0/copy";
+export { delay } from "jsr:@std/async@0.218.0/delay";
